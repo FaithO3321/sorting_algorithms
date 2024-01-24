0c5d6b3d4a20900ff11f9fa464e7eab0bc56bd86
FaithO3321 <faithomondi1331@gmail.com>
2024-01-23 17:52:58 -0800

    count algorithm

---
+++ 102-counting_sort.c
@@ -0,0 +1,63 @@
+#include "sort.h"
+/**
+ * get_max - find the maximum value in an array
+ * @array: array of integers
+ * @size: size of the array.
+ * Return: maximum integer of the array
+ */
+int get_max(int *array, int size)
+{
+	int max, a;
+
+	for (max = array[0], a = 1; a < size; a++)
+	{
+		if (array[a] > max)
+			max = array[a];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort – counting to sort an array of integers in ascending order
+ * @array: An array of integers
+ * @size: size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *count, *sorted, max, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+		return;
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(sorted);
+		return;
+	}
+
+	for (j = 0; j < (max + 1); j++)
+		count[j] = 0;
+	for (j = 0; j < (int)size; j++)
+		count[array[j]] += 1;
+	for (j = 0; j < (max + 1); j++)
+		count[j] += count[j - 1];
+	print_array(count, max + 1);
+
+	for (j = 0; j < (int)size; j++)
+	{
+		sorted[count[array[j]] - 1] = array[j];
+		count[array[j]] -= 1;
+	}
+
+	for (j = 0; j < (int)size; j++)
+		array[j] = sorted[j];
+
+	free(sorted);
+	free(count);
+}
